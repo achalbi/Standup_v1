668f8df1a905a03e5ef42952a9748c640a0e4cc2
achalbi <iachal@smartzip.com>
2018-01-13 09:27:33 +0530

    fixed landing page alert

--- lib/standup_web/controllers/page_controller.ex
+++ lib/standup_web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule StandupWeb.PageController do
   use StandupWeb, :controller
 
+  alias Standup.Accounts
   def index(conn, _params) do
+    cond do
+      Accounts.logged_in?(conn) -> render(conn, "index.html")
+      true -> conn |> redirect(to: "/auth/identity")
+    end 
     render conn, "index.html"
   end
 end
--- lib/standup_web/router.ex
+++ lib/standup_web/router.ex
@@ -51,12 +51,12 @@ defmodule StandupWeb.Router do
     pipe_through [:browser, :browser_session]
     
     resources "/users", UserController, only: [:new, :create]
+    get "/", PageController, :index
   end
 
   scope "/", StandupWeb do
     pipe_through [:browser, :browser_session, :login_required] # Use the default browser stack
     
-    get "/", PageController, :index
     resources "/users", UserController, except: [:new, :create]
     resources "/roles", RoleController
     resources "/organizations", OrganizationController
