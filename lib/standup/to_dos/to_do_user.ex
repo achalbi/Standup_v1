dea0ed21119793bde7593e72f4737431a8498cf5
achalbi <iachal@smartzip.com>
2018-03-03 11:52:58 +0530

    ui color fix

--- lib/standup/to_dos/to_do_user.ex
+++ lib/standup/to_dos/to_do_user.ex
@@ -2,11 +2,13 @@ defmodule Standup.ToDos.ToDoUser do
   use Ecto.Schema
   import Ecto.Changeset
   alias Standup.ToDos.ToDoUser
+  alias Standup.ToDos.ToDo
+  alias Standup.Accounts.User
 
 
   schema "to_do_users" do
-    field :to_do_id, :id
-    field :user_id, :id
+    belongs_to :to_do, ToDo
+    belongs_to :user, User
 
     timestamps()
   end
@@ -14,7 +16,7 @@ defmodule Standup.ToDos.ToDoUser do
   @doc false
   def changeset(%ToDoUser{} = to_do_user, attrs) do
     to_do_user
-    |> cast(attrs, [])
+    |> cast(attrs, [:to_do_id, :user_id])
     |> validate_required([])
   end
 end
