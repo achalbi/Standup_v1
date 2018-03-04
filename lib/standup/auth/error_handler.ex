87b566cfd14e406b6cd22b527eb6445423f7f773
achalbi <iachal@smartzip.com>
2018-03-04 12:26:45 +0530

    login  UI minor fix

--- lib/standup/auth/error_handler.ex
+++ lib/standup/auth/error_handler.ex
@@ -5,7 +5,8 @@ defmodule Standup.Auth.ErrorHandler do
     #body = Poison.encode!(%{message: to_string(type)})
     #send_resp(conn, 401, body)
     conn
-    |> Phoenix.Controller.put_flash(:error, "You must be signed in to access that page.")
+   # |> Phoenix.Controller.put_flash(:error, "You must be signed in to access that page.")
+    |> Phoenix.Controller.put_flash(:info, "Please sign into Standup.")
     |> Phoenix.Controller.redirect(to: "/auth/identity", callback_url: Helpers.callback_url(conn)) 
     |> Plug.Conn.halt()
   end
