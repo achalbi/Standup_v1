02b17880889ec8c9be2e099f7fb7b58311d6f34d
achalbi <iachal@smartzip.com>
2018-03-09 11:49:51 +0530

    ui fixes minor

--- lib/standup/authorizers/authorizer.ex
+++ lib/standup/authorizers/authorizer.ex
@@ -1,17 +1,17 @@
 defmodule Standup.Plugs.Authorizer do
 	use StandupWeb, :controller
 
-  require IEx
   def unauthorized_user(conn) do
     [url] = get_req_header(conn, "referer")
     key = "x-pjax-url"
-    value = url
-    conn = delete_req_header(conn, "x-pjax")
-    conn = delete_req_header(conn, "x-pjax-container")
-    conn = put_resp_header(conn, key, value)
+   # conn = delete_req_header(conn, "x-pjax")
+  #  conn = delete_req_header(conn, "x-pjax-container")
+    conn = put_resp_header(conn, key, url)
+    flash = Map.put(get_flash(conn), "error", "You do not have permission for this request")
+    conn = put_session(conn, "pjax_flash", flash)
       conn
         |> put_status(403)
-      #  |> send_resp(403, "You do not have permission for this request")
+      # |> send_resp(403, "You do not have permission for this request")
         |> put_flash(:error, "You do not have permission for this request")
       #  |> render(StandupWeb.ErrorView, :"403")
         |> redirect(external: url)
